e28728e8004790e27d9ff9234b5c1c8a5bf94de7
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2023-06-29 14:32:41 -0300

    Relatorio

--- Main.java
+++ Main.java
@@ -19,18 +19,9 @@ public class Main extends Funcoes{
 
         Registro[] baseDeDados = construirVetorRegistro(caminho2);
         //Registro[] baseDeDados = construirVetorMenor(caminhoTeste); //Vetor menor para testar as ordenações
-        
-        InsertionSort objInsertionSort = new InsertionSort();
-        objInsertionSort.gerarInsertionSort(baseDeDados);
-
-        SelectionSort objSelectionSort = new SelectionSort();
-        objSelectionSort.gerarSelectionSort(baseDeDados);
 
         QuickSort objQuickSort = new QuickSort();
         objQuickSort.gerarQuickSort(baseDeDados);
-
-        QuickSortMedianaDe3 objQuickSortMedianaDe3 = new QuickSortMedianaDe3();
-        objQuickSortMedianaDe3.gerarQuickSortMed3(baseDeDados);
 
         MergeSort objMergeSort = new MergeSort();
         objMergeSort.gerarMergeSort(baseDeDados);
@@ -38,8 +29,6 @@ public class Main extends Funcoes{
         HeapSort objHeapSort = new HeapSort();
         objHeapSort.gerarHeapSort(baseDeDados);
 
-        CountingSort objCountingSort = new CountingSort();
-        objCountingSort.gerarcountingSort(baseDeDados);
         
 
     }//Fim do main
